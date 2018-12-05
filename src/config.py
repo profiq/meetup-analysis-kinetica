42ed3f2c1211eeef2099cd62685a492bfe50b9a2
Milos Svana <milos.svana@profiq.com>
2018-12-05 14:37:52 +0100

    Add table creation to Python

--- src/config.py
+++ src/config.py
@@ -3,9 +3,26 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SRC_DIR = os.path.join(BASE_DIR, 'src')
 
-# Kinetica connetcion parameters
+# Kinetica connection parameters
 GPUDB_HOST = 'kinetica'
 GPUDB_PORT = '9191'
+
+# Table definition for storing responses to Meetup events
+EVENT_RSVP_COLLECTION = 'meetup'
+EVENT_RSVP_TABLE_NAME = 'event_rsvp'
+
+EVENT_RSVP_TYPE = [
+    ['event_id', 'string', 'char32', 'primary_key', 'shard_key'],
+    ['name', 'string', 'char128', 'text_search'],
+    ['url', 'string', 'char128'],
+    ['event_timestamp', 'long', 'nullable', 'timestamp'],
+    ['lat', 'double', 'nullable'],
+    ['lon', 'double', 'nullable'],
+    ['rsvp_id', 'long', 'primary_key'],
+    ['response', 'int', 'int8'],
+    ['rsvp_timestamp', 'long', 'timestamp']
+]
+
 
 # UDF for collecting Meetup.com data
 UDF_NAME = 'collect_meetup'
--- src/deploy.py
+++ src/deploy.py
@@ -6,13 +6,20 @@ import config
 def main():
     db = gpudb.GPUdb(host=config.GPUDB_HOST, port=config.GPUDB_PORT)
 
+    gpudb.GPUdbTable(
+        _type=config.EVENT_RSVP_TYPE,
+        name=config.EVENT_RSVP_TABLE_NAME,
+        options={'collection_name': config.EVENT_RSVP_COLLECTION},
+        db=db)
+
+    '''
     db.create_proc(
         proc_name=config.UDF_NAME,
         execution_mode=config.UDF_EXECUTION_MODE,
         files=config.UDF_FILES,
         command=config.UDF_COMMAND,
         args=config.UDF_FILES)
-
+    '''
 
 if __name__ == '__main__':
     main()
