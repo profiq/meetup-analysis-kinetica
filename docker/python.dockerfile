5d5181ca6f77afce5d90f125fcc45e3fa56d1723
Milos Svana <milos.svana@profiq.com>
2018-12-05 14:56:28 +0100

    Add automatic table creation and meetup data collection to docker

--- docker/python.dockerfile
+++ docker/python.dockerfile
@@ -4,9 +4,9 @@ RUN apt-get update && apt-get install -y \
     python-dev \
     python-pip
 
-RUN pip install -r /app/requirements.txt
-
 COPY . /app
 WORKDIR /app
 
-CMD python src/meetup.py
+RUN pip install -r /app/requirements.txt
+
+ENTRYPOINT ["/bin/sh", "-c", "python /app/src/deploy.py && python /app/src/meetup.py"]
--- src/deploy.py
+++ src/deploy.py
@@ -6,20 +6,18 @@ import config
 def main():
     db = gpudb.GPUdb(host=config.GPUDB_HOST, port=config.GPUDB_PORT)
 
-    gpudb.GPUdbTable(
-        _type=config.EVENT_RSVP_TYPE,
-        name=config.EVENT_RSVP_TABLE_NAME,
-        options={'collection_name': config.EVENT_RSVP_COLLECTION},
-        db=db)
+    try:
+        gpudb.GPUdbTable(
+            _type=config.EVENT_RSVP_TYPE,
+            name=config.EVENT_RSVP_TABLE_NAME,
+            options={'collection_name': config.EVENT_RSVP_COLLECTION},
+            db=db)
+    except gpudb.GPUdbException as e:
+        if "Table '%s' exists;" % config.EVENT_RSVP_TABLE_NAME in str(e):
+            print('Table for Meetup events already exists')
+        else:
+            raise e
 
-    '''
-    db.create_proc(
-        proc_name=config.UDF_NAME,
-        execution_mode=config.UDF_EXECUTION_MODE,
-        files=config.UDF_FILES,
-        command=config.UDF_COMMAND,
-        args=config.UDF_FILES)
-    '''
 
 if __name__ == '__main__':
     main()
