3e9e7a394037fb784e283dce9ae41c72f59f9429
Milos Svana <milos.svana@profiq.com>
2018-12-14 15:55:15 +0100

    Improve error handling when getting city info

--- src/apiutils.py
+++ src/apiutils.py
@@ -1,7 +1,24 @@
+import json
 import time
 
+import requests
 
-class MeetupThrottle:
+
+class VerboseMixin:
+    LOG_PREFIX = ''
+
+    def _print(self, msg):
+        """
+        :param str msg: Message to be printed
+        """
+        assert hasattr(self, '_verbose')
+        if self._verbose:
+            print('[%s] %s' % (self.LOG_PREFIX, msg.encode().decode(errors='replace')))
+
+
+class MeetupThrottle(VerboseMixin):
+    LOG_PREFIX = 'throttle'
+
     """
     Meetup.com API allows for only `max_requests` requests during `period` seconds long period of time
     This class can be used to control number of API requests in other parts of code.
@@ -30,9 +47,9 @@ class MeetupThrottle:
 
     def increment_counter(self):
         self._counter += 1
+        self._print('Requests in this period: %d' % self._counter)
 
     def wait_for_request_permission(self):
-        self._print('Cnt: %d' % self._counter)
         if self._counter == self._max_requests:
             self._print('Counter reached maximum')
             time_from_last_reset = time.time() - self._last_period_reset_time
@@ -42,9 +59,94 @@ class MeetupThrottle:
                 time.sleep(time_to_sleep)
             self._counter = 0
 
-    def _print(self, msg):
+
+class CityInfoProvider(VerboseMixin):
+    LOG_PREFIX = 'city'
+
+    def __init__(self, endpoint, db, table_name, throttle, verbose=False):
         """
-        :param str msg: Message to be printed
+        :param str endpoint: Meetup.com API endpoint URL for getting city info from coordinates
+        :param gpudb.GPUdb db: Connection to Kinetica DB
+        :param str table_name: Meetup RSVPs table name
+        :param MeetupThrottle throttle: Throttle object to manage API request limits
+        :param bool verbose: Print debug output
         """
-        if self._verbose:
-            print('[throttle] %s' % msg)
+        self._endpoint = endpoint
+        self._db = db
+        self._table_name = table_name
+        self._throttle = throttle
+        self._verbose = verbose
+
+    def get_city_for_coordinates(self, event_id, lat, lon):
+        """
+        :param str event_id:
+        :param float lat:
+        :param float lon:
+        :rtype: str
+        """
+        if lat is None or lon is None or lat == 0.0 or len == 0.0:
+            city = None
+        else:
+            city = self._find_city_in_db(event_id)
+            if city is None:
+                city = self._find_city_on_meetup(lat, lon)
+        return city
+
+    def _find_city_on_meetup(self, lat: float, lon: float):
+        """
+        :param float lat:
+        :param float lon:
+        :rtype: str
+        """
+        self._throttle.wait_for_request_permission()
+        params = {'lat': lat, 'lon': lon, 'page': 1}
+        response = requests.get(self._endpoint, params)
+        self._throttle.increment_counter()
+
+        if len(response.content) == 0 or response.status_code != 200:
+            self._print('Something wrong with the response from meetup.com')
+            self._print('Status code: %d' % response.status_code)
+            self._print('Text: %s' % response.text)
+            self._print('Lat: %.6f, Lon: %.6f' % (lat, lon))
+            city = None
+        else:
+            response_json = response.json()
+            if len(response_json['results']) == 1:
+                city = response_json['results'][0]['city']
+                self._print('City found for lat %.6f and lon %.6f using Meetup API' % (lat, lon))
+            else:
+                city = None
+                self._print('City not found for lat %.6f and lon %.6f' % (lat, lon))
+        return city
+
+    def _find_city_in_db(self, event_id):
+        """
+        :param str event_id:
+        :rtype: str
+        """
+        try:
+            results = self._db.get_records(
+                self._table_name,
+                limit=2,
+                encoding='json',
+                options={'expression': 'event_id = "%s" AND IS_NULL(city) = 0' % event_id})
+
+            status = results['status_info']
+            num_records = len(results['records_json'])
+
+            if status['status'] == 'ERROR':
+                self._print('Something went wrong with the DB')
+                self._print(status['message'])
+                city = None
+            elif num_records > 0:
+                rsvp_record = json.loads(results['records_json'][0])
+                city = rsvp_record['city']
+                self._print('City found for event %s in the DB' % event_id)
+            else:
+                city = None
+                self._print('City not found for event %s' % event_id)
+
+        except UnicodeDecodeError:
+            city = None
+
+        return city
--- src/meetup.py
+++ src/meetup.py
@@ -4,88 +4,27 @@ import json
 import traceback
 
 import gpudb
-import requests
 import websocket
 
 import apiutils
 import config
 
 
-class CityInfoProvider:
-
-    def __init__(self, endpoint: str, db: gpudb.GPUdb, table_name: str, throttle: apiutils.MeetupThrottle,
-                 verbose=False):
-        self._endpoint = endpoint
-        self._db = db
-        self._verbose = verbose
-        self._table_name = table_name
-        self._throttle = throttle
-
-    def get_city_for_coordinates(self, event_id: str, lat: float, lon: float) -> str:
-        if lat is None or lon is None:
-            city = None
-        else:
-            city = self._find_city_in_db(event_id)
-            if city is None:
-                city = self._find_city_on_meetup(lat, lon)
-        return city
-
-    def _find_city_on_meetup(self, lat: float, lon: float) -> str:
-        self._throttle.wait_for_request_permission()
-        params = {'lat': lat, 'lon': lon, 'page': 1}
-        response = requests.get(self._endpoint, params).json()
-        self._throttle.increment_counter()
-        if 'results' in response and len(response['results']) == 1:
-            city = response['results'][0]['city']
-        else:
-            city = None
-        return city
-
-    def _find_city_in_db(self, event_id: str) -> str:
-        city = None
-
-        try:
-            results = self._db.get_records(
-                self._table_name,
-                limit=2,
-                encoding='json',
-                options={'expression': 'event_id = "%s" AND IS_NULL(city) = 0' % event_id})
-        except UnicodeDecodeError:
-            return None
-
-        status = results['status_info']
-        num_records = len(results['records_json'])
-
-        if num_records > 0:
-            rsvp_record = json.loads(results['records_json'][0])
-            city = rsvp_record['city']
-
-        if self._verbose:
-            if num_records == 0:
-                print('Event with city not found, need to use Meetup API')
-            elif num_records > 0:
-                print('City found in the DB')
-        if status['status'] == 'ERROR':
-            raise Exception(status['message'])
-
-        return city
-
-
 def main():
     db = gpudb.GPUdb(host=config.GPUDB_HOST, port=config.GPUDB_PORT)
     throttle = apiutils.MeetupThrottle(config.MEETUP_MAX_REQUESTS, config.MEETUP_PERIOD)
-    city_info_provider = CityInfoProvider(
-        config.MEETUP_API_CITIES_ENDPOINT, db, config.EVENT_RSVP_TABLE_NAME, throttle, True)
+    city_info_provider = apiutils.CityInfoProvider(
+        config.MEETUP_API_CITIES_ENDPOINT, db, config.EVENT_RSVP_TABLE_NAME, throttle)
     websocket.enableTrace(False)
     on_message = functools.partial(store_rsvp, db=db, city_info_provider=city_info_provider)
     ws = websocket.WebSocketApp(config.MEETUP_API_RSVP_ENDPOINT, on_message=on_message)
     ws.run_forever()
 
 
-def store_rsvp(_, rsvp_string, db, city_info_provider: CityInfoProvider):
+def store_rsvp(_, rsvp_string, db, city_info_provider: apiutils.CityInfoProvider):
     try:
         rsvp = json.loads(rsvp_string)
-        print('RSVP ID: %d ready to be processed' % rsvp['rsvp_id'])
+        print('[stream] RSVP ID: %d ready to be processed' % rsvp['rsvp_id'])
 
         rsvp_record = collections.OrderedDict()
         rsvp_record['event_id'] = rsvp['event']['event_id']
@@ -103,9 +42,9 @@ def store_rsvp(_, rsvp_string, db, city_info_provider: CityInfoProvider):
         response = db.insert_records('event_rsvp', json.dumps(rsvp_record, ensure_ascii=False), list_encoding='json')
 
         if response['status_info']['status'] == 'OK':
-            print('RSVP ID: %d stored in DB' % rsvp['rsvp_id'])
+            print('[stream] RSVP ID: %d stored in DB' % rsvp['rsvp_id'])
         else:
-            print('Error while storing')
+            print('[stream] Error while storing')
             print(response)
     except Exception as e:
         print(traceback.format_exc())
