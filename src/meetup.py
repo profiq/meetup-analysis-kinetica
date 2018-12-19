0e2eb4c874c353b541c81ffbe7c0e39110abfc70
Milos Svana <milos.svana@gmail.com>
2018-12-19 17:17:56 +0100

    Add documentation

--- src/meetup.py
+++ src/meetup.py
@@ -11,17 +11,27 @@ import config
 
 
 def main():
+    """
+    Start listening to the Meetup.com RSVP stream (uses WebSockets).
+    If a RSVP is received store it in the database.
+    """
     db = gpudb.GPUdb(host=config.GPUDB_HOST, port=config.GPUDB_PORT)
     throttle = apiutils.MeetupThrottle(config.MEETUP_MAX_REQUESTS, config.MEETUP_PERIOD)
-    city_info_provider = apiutils.CityProvider(
-        config.MEETUP_API_CITIES_ENDPOINT, db, config.EVENT_RSVP_TABLE_NAME, throttle)
+    city_provider = apiutils.CityProvider(config.MEETUP_API_CITIES_ENDPOINT, db, config.EVENT_RSVP_TABLE_NAME, throttle)
     websocket.enableTrace(False)
-    on_message = functools.partial(store_rsvp, db=db, city_info_provider=city_info_provider)
+    on_message = functools.partial(store_rsvp, db=db, city_info_provider=city_provider)
     ws = websocket.WebSocketApp(config.MEETUP_API_RSVP_ENDPOINT, on_message=on_message)
     ws.run_forever()
 
 
-def store_rsvp(_, rsvp_string, db, city_info_provider: apiutils.CityProvider):
+def store_rsvp(_, rsvp_string, db, city_provider):
+    """
+    Parses RSVP received from Meetup WebSocket and stores it in the DB
+
+    :param str rsvp_string: Raw message received from Meetup.com socket (JSON string)
+    :param db: Connection to Kinetica DB
+    :param apiutils.CityProvider city_provider: Finds city name using event ID + data in DB or coordinates + Meetup API
+    """
     try:
         rsvp = json.loads(rsvp_string)
         print('[stream] RSVP ID: %d ready to be processed' % rsvp['rsvp_id'])
@@ -36,7 +46,7 @@ def store_rsvp(_, rsvp_string, db, city_info_provider: apiutils.CityProvider):
         rsvp_record['rsvp_id'] = rsvp['rsvp_id']
         rsvp_record['response'] = 1 if rsvp['response'] == 'yes' else 0
         rsvp_record['rsvp_timestamp'] = rsvp['mtime']
-        rsvp_record['city'] = city_info_provider.get_city(
+        rsvp_record['city'] = city_provider.get_city(
             rsvp_record['event_id'], rsvp_record['lat'], rsvp_record['lon'])
 
         response = db.insert_records('event_rsvp', json.dumps(rsvp_record, ensure_ascii=False), list_encoding='json')
