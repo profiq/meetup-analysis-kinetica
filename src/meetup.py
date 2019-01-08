bea7f5cf886fb4f944d45a389a124078482a9e56
Milos Svana <milos.svana@profiq.com>
2019-01-08 16:11:48 +0100

    Fix broken RSVP processing caused by incorrect variable names

--- src/meetup.py
+++ src/meetup.py
@@ -2,6 +2,7 @@ import collections
 import functools
 import json
 import traceback
+import sys
 
 import gpudb
 import websocket
@@ -24,14 +25,15 @@ def main():
     ws.run_forever()
 
 
-def store_rsvp(_, rsvp_string, db, city_provider):
+def store_rsvp(_, rsvp_string, db, city_info_provider):
     """
     Parses RSVP received from Meetup WebSocket and stores it in the DB
 
     :param str rsvp_string: Raw message received from Meetup.com socket (JSON string)
     :param db: Connection to Kinetica DB
-    :param apiutils.CityProvider city_provider: Finds city name using event ID + data in DB or coordinates + Meetup API
+    :param apiutils.CityProvider city_info_provider: Finds city name using event ID + data in DB or coordinates + Meetup API
     """
+
     try:
         rsvp = json.loads(rsvp_string)
         print('[stream] RSVP ID: %d ready to be processed' % rsvp['rsvp_id'])
@@ -46,7 +48,7 @@ def store_rsvp(_, rsvp_string, db, city_provider):
         rsvp_record['rsvp_id'] = rsvp['rsvp_id']
         rsvp_record['response'] = 1 if rsvp['response'] == 'yes' else 0
         rsvp_record['rsvp_timestamp'] = rsvp['mtime']
-        rsvp_record['city'] = city_provider.get_city(
+        rsvp_record['city'] = city_info_provider.get_city(
             rsvp_record['event_id'], rsvp_record['lat'], rsvp_record['lon'])
 
         response = db.insert_records('event_rsvp', json.dumps(rsvp_record, ensure_ascii=False), list_encoding='json')
@@ -56,6 +58,8 @@ def store_rsvp(_, rsvp_string, db, city_provider):
         else:
             print('[stream] Error while storing')
             print(response)
+    except KeyboardInterrupt:
+        sys.exit(0)
     except Exception as e:
         print(traceback.format_exc())
         raise e
