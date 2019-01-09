2a091bff08d66ae12bb0b61aebdd60ff02243701
Milos Svana <milos.svana@profiq.com>
2019-01-09 16:41:39 +0100

    Add additional information about events

--- src/prediction.py
+++ src/prediction.py
@@ -9,8 +9,10 @@ from sklearn import model_selection
 from sklearn import neural_network
 from sklearn import preprocessing
 from tzwhere import tzwhere
+import requests
 
 import config
+import apiutils
 
 
 def main():
@@ -21,11 +23,21 @@ def main():
 
     timezones = events_df.apply(lambda e: tzfinder.tzNameAt(e['lat'], e['lon']), axis=1)
     events_df['timezone'] = timezones
+
+    throttle = apiutils.MeetupThrottle(config.MEETUP_MAX_REQUESTS, config.MEETUP_PERIOD)
+    event_info_provider = EventInfoProvider(throttle)
+    events_df['event_info'] = events_df.apply(
+        lambda e: event_info_provider.get_info_for_event(e['event_id']), axis=1)
+
     events_df.dropna(inplace=True)
+
     events_df['timezone'] = events_df.apply(lambda e: pytz.timezone(e['timezone']), axis=1)
     events_df['time_local'] = events_df.apply(lambda e: e['time_utc'].astimezone(e['timezone']), axis=1)
     events_df['day_of_week'] = events_df.apply(lambda e: e['time_local'].weekday(), axis=1)
     events_df['hour'] = events_df.apply(lambda e: e['time_local'].hour, axis=1)
+    events_df['group_members'] = events_df.apply(lambda e: e['event_info']['group_members'], axis=1)
+    events_df['past_events'] = events_df.apply(lambda e: e['event_info']['group_past_event_count'], axis=1)
+    events_df['country'] = events_df.apply(lambda e: e['event_info']['country'], axis=1)
 
     train_model(events_df)
 
@@ -39,7 +51,7 @@ def get_events_from_db(db, offset=0):
     events_response = db.aggregate_group_by(
         table_name=config.EVENT_RSVP_TABLE_NAME,
         column_names=['city', 'event_id', 'event_timestamp', 'SUM(response) AS yes_responses', 'lat', 'lon'],
-        limit=30000,
+        limit=10,
         offset=offset,
         encoding='json',
         options={
@@ -87,6 +99,42 @@ def train_model(events):
     y_pred = cls.predict(X_train)
     score = metrics.r2_score(y_train, y_pred)
     print(score)
+
+
+class EventInfoProvider:
+
+    def __init__(self, throttle):
+        """
+        :param apiutils.MeetupThrottle throttle: Throttle to limit number of requests to Meetup.com API
+        """
+        self._throttle = throttle
+        self._cache = {}
+
+    def get_info_for_event(self, event_id):
+        """
+        :param str event_id: ID of the event you want to know more about
+        :rtype: dict
+        """
+        if event_id not in self._cache:
+            self._throttle.wait_for_request_permission()
+            params = {'event_id': event_id}
+            response = requests.get('https://api.meetup.com/2/events', params=params).json()
+            self._throttle.increment_counter()
+
+            if len(response['results']) > 0:
+                group_urlname = response['results'][0]['group']['urlname']
+                self._throttle.wait_for_request_permission()
+                response_group = requests.get('https://api.meetup.com/%s' % group_urlname,
+                                              params={'fields': 'past_event_count'}).json()
+                self._throttle.increment_counter()
+                self._cache[event_id] = {
+                    'country': response['results'][0]['venue']['country'],
+                    'group_members': response_group['members'],
+                    'group_past_event_count': response_group['past_event_count']
+                }
+            else:
+                self._cache[event_id] = None
+        return self._cache[event_id]
 
 
 if __name__ == '__main__':
